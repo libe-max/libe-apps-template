46eb0b55e43e674e7efc26486ba98503fba68c87
Maxime Fabas <m.fabas@liberation.fr>
2018-11-13 11:13:26 +0100

    Re-organize the components

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
-import config from './config'
-
-const SomeApp = props => <div className="some-app">
-  <pre>{JSON.stringify(props, null, 2)}</pre>
-</div>
-
-const LibeLaboAppWrapper = props => <div
-  className="libe-labo-app-wrapper">
-  <SomeApp {...config} />
-</div>
+import App from './App'
 
 ReactDOM.render(
-  <LibeLaboAppWrapper />,
+  <App />,
   document.getElementById('react-app-root')
 )
 
