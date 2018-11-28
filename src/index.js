f6008a13d33c5f2ff303e744135fa8c2cf7bd539
Maxime Fabas <m.fabas@liberation.fr>
2018-11-28 12:16:05 +0100

    Simplification of the setup files

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import config from './config.json'
 
-const App = () => <div className="libe-labo-app-wrapper">
-  {/* Here: your app component with config.js as props */}
+const App = () => <div {...config}>
+  {/* Replace this App component with your app */}
 </div>
 
 ReactDOM.render(
   <App />,
-  document.getElementById('react-app-root')
+  document.getElementById('libe-labo-app-wrapper')
 )
 
 serviceWorker.unregister()
