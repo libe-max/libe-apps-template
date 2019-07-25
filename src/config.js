806b3d36c6842495f8b85d326cff08f071e1b724
Maxime Fabas <m.fabas@liberation.fr>
2019-07-25 14:22:59 +0200

    Turn libe-labo.site into libe-labo-2.site

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ module.exports = {
     ? 'https://www.liberation.fr/apps/static'
     : 'http://localhost:3003',
   api_url: process.env.NODE_ENV === 'production'
-    ? 'https://libe-labo.site/api'
+    ? 'https://libe-labo-2.site/api'
     : 'http://localhost:3004/api',
   stylesheet: 'libe-apps-template.css' // The name of the css file hosted at ${statics_url}/styles/apps/
 }
