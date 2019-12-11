f9dfe43f8c2ce1af843974cff4e4d449a2fe43e6
Maxime Fabas <maxime.fabas@pm.me>
2019-12-11 17:26:24 +0100

    fix wrong static url when away from localhost

--- src/config.js
+++ src/config.js
@@ -16,10 +16,10 @@ const config = {
   show_header: true,
   statics_url: process.env.NODE_ENV === 'production'
     ? 'https://www.liberation.fr/apps/static'
-    : 'http://localhost:3003',
+    : `${window.location.protocol}//${window.location.hostname}:3003`,
   api_url: process.env.NODE_ENV === 'production'
     ? 'https://libe-labo-2.site/api'
-    : 'http://localhost:3004/api',
+    : `${window.location.protocol}//${window.location.hostname}:3004/api`,
   stylesheet: 'libe-apps-template.css', // The name of the css file hosted at ${statics_url}/styles/apps/
   spreadsheet: undefined // The spreadsheet providing data to the app
 }
