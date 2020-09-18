72fab0614f9b9ef3e780d9cb06ce443dd84033ab
maximefabas (work) <maxime.fabas@protonmail.com>
2020-09-18 11:08:18 +0200

    add new libelabo logger script

--- build.js
+++ build.js
@@ -38,6 +38,7 @@ async function build () {
 
   // Add meta tags
   console.log('\nAdding meta tags')
+  const { statics_url: staticsUrl } = config
   const { title, url, description, author, image, xiti_id: xitiId } = config.meta
   const $ = cheerio.load(fileData)
   $('head').append(`
@@ -114,6 +115,9 @@ async function build () {
       <img width="1" height="1" src="https://logliberation.xiti.com/hit.xiti?s=381060&amp;s2=56&amp;p=LibeLabo::${xitiId}&amp;di=&amp;" >
     </noscript>
     <script type="text/javascript" src="https://statics.liberation.fr/bloom/theme/js/xtcore.js"></script>
+
+    <!-- Libe Labo logger -->
+    <script type="text/javascript" src="${staticsUrl}/scripts/logger.js"></script>
   `)
 
   // Save index.html
--- src/config.js
+++ src/config.js
@@ -20,9 +20,6 @@ const config = {
   statics_url: process.env.NODE_ENV === 'production'
     ? 'https://www.liberation.fr/apps/static'
     : `${currentProtocol}//${currentHostname}:3003`,
-  api_url: process.env.NODE_ENV === 'production'
-    ? 'https://libe-labo-2.site/api'
-    : `${currentProtocol}//${currentHostname}:3004/api`,
   stylesheet: 'libe-apps-template.css', // The name of the css file hosted at ${statics_url}/styles/apps/
   spreadsheet: undefined // The spreadsheet providing data to the app
 }
